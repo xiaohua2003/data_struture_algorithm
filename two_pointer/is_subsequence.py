7d4b7ada2c51d91538e59fd3a23e8c824aa5bc4b
Xiaohua Cai <xcai3@ualberta.ca>
2026-09-21 11:17:49 -0700

    add the second approach

--- two_pointer/is_subsequence.py
+++ two_pointer/is_subsequence.py
@@ -7,3 +7,15 @@ class Solution:
         return s_pos == len(s)
 
 # approach 2：
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        sp = 0
+        tp = 0
+        while sp < len(s) and tp < len(t):
+            if s[sp] == t[tp]:
+                sp += 1
+            tp += 1
+        return sp == len(s)
+     
+
+         
