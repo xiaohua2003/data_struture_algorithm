2169fc1d28aaf8ae92e522f716a13b33bdd494e6
Xiaohua Cai <xcai3@ualberta.ca>
2026-09-22 10:49:22 -0700

    size of the longest non-empty subarray containing only 1's in the resulting array

---
+++ sliding_windows/delet_one_element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestSubarray(self, nums: list[int]) -> int:
+        l = 0
+        zero_count = 0
+        res = 0
+        for r in range(len(nums)):
+            if nums[r] == 0:
+                zero_count += 1
+            while zero_count > 1:
+                if nums[l] == 0:
+                    zero_count -= 1
+                l += 1
+            res = max(res, r - l)
+        return res
+    
+
+        
