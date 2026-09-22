2747e2f05337af51544b2402438fbaeca9531239
Xiaohua Cai <xcai3@ualberta.ca>
2026-09-21 21:41:39 -0700

    implement find maximum average sub array

--- sliding_windows/max_average_subarray.py
+++ sliding_windows/max_average_subarray.py
@@ -8,8 +8,24 @@ class Solution:
             cur_sum -= nums[i - 1]
             res = max(res, cur_sum)
         return res / k
+
+# approach 2
+
+
+class Solution:
+    def findMaxAverage(self, nums: list[int], k: int) -> float:
+        cur_sum = sum(nums[:k])
+        res = cur_sum
+        for i in range(k, len(nums)):
+            cur_sum += nums[i] - nums[i - k]
+            res = max(res, cur_sum)
+        return res / k
+   
         
 
+      
+            
+        
       
             
         
