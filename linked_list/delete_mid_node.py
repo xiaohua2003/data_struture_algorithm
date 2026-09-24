d4a0dd092bedd445d8209f44881afc0f3b064d0c
Xiaohua Cai <xcai3@ualberta.ca>
2026-09-24 11:16:56 -0700

    implement delete mid node

--- linked_list/delete_mid_node.py
+++ linked_list/delete_mid_node.py
@@ -24,8 +24,47 @@ class Solution:
             i += 1
         return dummy.next
 
+#approach 2
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteMiddle(self, head: ListNode | None) -> ListNode | None:
+        if not head or not head.next:
+            return None
+        fast = head
+        slow = head
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+        prev.next = slow.next
+        return head
 
  
+       
+
+
+ 
+    
+
+
+  
+        
+
+
+
+
+
+        
+            
+
+
+
+        
     
 
 
