e23af74a16c258148b9e6aafad676eb69c7d1277
Xiaohua Cai <xcai3@ualberta.ca>
2026-09-25 10:24:17 -0700

    implement odd even list

---
+++ linked_list/odd_even_list(mid).py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: ListNode | None) -> ListNode | None:
+        if not head or not head.next:
+            return head
+        odd = head
+        even = head.next
+        even_temp = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+
+            even.next = odd.next
+            even = even.next
+        odd.next = even_temp
+        return head
+
+
+     
+    
+      
+
+
+
+        
